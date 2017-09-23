8b60e500b3a3f83eda7d343012aef952bd091830
Edvin Syse <es@syse.no>
2017-09-23 19:34:47 +0200

    Model uses IntegerProperty instead of StringProperty to avoid casting from String when incrementing.

--- griffon-app/controllers/org/example/SampleController.kt
+++ griffon-app/controllers/org/example/SampleController.kt
@@ -4,8 +4,8 @@ import griffon.core.artifact.GriffonController
 import griffon.core.controller.ControllerAction
 import griffon.inject.MVCMember
 import griffon.metadata.ArtifactProviderFor
-import org.codehaus.griffon.runtime.core.artifact.AbstractGriffonController
 import griffon.transform.Threading
+import org.codehaus.griffon.runtime.core.artifact.AbstractGriffonController
 import javax.annotation.Nonnull
 
 @ArtifactProviderFor(GriffonController::class)
@@ -16,7 +16,6 @@ class SampleController : AbstractGriffonController() {
     @ControllerAction
     @Threading(Threading.Policy.INSIDE_UITHREAD_ASYNC)
     fun click() {
-        val count = Integer.parseInt(model.clickCount)
-        model.clickCount = (count + 1).toString()
+        model.clickCount = model.clickCount + 1
     }
 }
--- griffon-app/models/org/example/SampleModel.kt
+++ griffon-app/models/org/example/SampleModel.kt
@@ -2,17 +2,14 @@ package org.example
 
 import griffon.core.artifact.GriffonModel
 import griffon.metadata.ArtifactProviderFor
-import javafx.beans.property.StringProperty
-import javafx.beans.property.SimpleStringProperty
+import javafx.beans.property.IntegerProperty
+import javafx.beans.property.SimpleIntegerProperty
 import org.codehaus.griffon.runtime.core.artifact.AbstractGriffonModel
+import tornadofx.*
 
 @ArtifactProviderFor(GriffonModel::class)
 class SampleModel : AbstractGriffonModel() {
-    private var _clickCount: StringProperty = SimpleStringProperty(this, "clickCount", "0")
-
-    var clickCount: String
-        get() = _clickCount.get()
-        set(s) = _clickCount.set(s)
-
+    private var _clickCount: IntegerProperty = SimpleIntegerProperty(this, "clickCount", 0)
+    var clickCount by _clickCount
     fun clickCountProperty() = _clickCount
 }
